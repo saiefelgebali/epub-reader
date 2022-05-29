d0c9824e9d43f95c036f2edddf78554f6ddee968
Saief El Gebali <saiefelsayed@gmail.com>
2022-05-29 20:36:11 +0100

    ADD: PWA Support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
 	content: ["./*.html", "./src/**/*.{ts,tsx}"],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"background-900": "#16191D",
+				"background-800": "#21252C",
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography")],
 };
